b2928d9f53a3019d2b39e3ab31c95f7971266b62
pulse <antynomia@gmail.com>
2016-09-21 21:02:46 +0200

    modified:   script.sh

--- script.sh
+++ script.sh
@@ -5,11 +5,15 @@ cat > ~/.ssh/config <<EOF
 Host bitbucket.org
   IdentityFile ~/.ssh/klucze/id-rsa
 Host bitbucket.otlabs.fr
-  IdentityFile ~/.ssh/klucze/id-rsa  
+  IdentityFile ~/.ssh/klucze/id-rsa
+  Host github.com
+    IdentityFile ~/.ssh/klucze/id-rsa
 EOF
 
 
 chmod 400 ~/.ssh/klucze/id-rsa
  ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
  ssh-keyscan bitbucket.otlabs.fr >> ~/.ssh/known_hosts
+ ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 /git-sync
